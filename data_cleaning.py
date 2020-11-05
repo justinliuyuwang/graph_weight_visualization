ac75676007aedb45ba6ac69e9a90a55a32dd6351
Justin Wang <justinwang@Justins-MacBook-Air.local>
2020-11-05 02:26:19 -0500

    commented

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+# This is a script that cleans and calculates mean & stdev from raw distance measurements between items.
 
 import glob
 import pandas as pd
@@ -10,72 +6,92 @@ import scipy.io as sp
 from statistics import mean, stdev
 from pathlib import Path
 
+#get filepaths for each of the categories' folders
 categories = glob.glob('inputs/rawdata/*_raw')
 
-my_filename = "inputs/cleandata/distance_data.csv"
-if not Path(my_filename).is_file():
-    f = open(my_filename, "x")
+
+outputfile = "inputs/cleandata/distance_data.csv"
+
+#create file if it doesn't exist
+if not Path(outputfile).is_file():
+    f = open(outputfile, "x")
     f.close()
 
+#flag marks whether it's the first time writing data to the output file
 flag = 0
 
+#for each category
 for directory_path in categories:
 
+    #category name extracted from folder path
     category = directory_path.replace("inputs/rawdata/", "").replace("_raw", "")
 
+    #list of files in the category folder
     files = glob.glob(directory_path + '/*.mat')
 
+    #dictionary to hold each item pair's list of distance measurements
     myDict = {}
 
+    #for each file (participant response for this category)
     for file_path in files:
 
+        #load matlab data
         mat_contents = sp.loadmat(file_path)
         data = mat_contents
 
+        #clean \ufeff and whitespace from item names
         for index in range(len(data["stimuli"])):
             data["stimuli"][index] = data["stimuli"][index].replace("\ufeff", "").replace(" ", "")
 
-        # print(data["stimuli"])
-        # print(data["rdmutv"])
-
-        # for index in range(len(data["rdmutv"][0])):
-        #    print(data["rdmutv"][0][index])
 
         n = len(data["stimuli"])
         j = 0
         k = 1
         m = 0
 
+        #iterate through all possible pairs of items and assign the appropriate measurements to each combination
+        #j is the index of the first item and k is the index of the second item
+        #m is the index of the measurement
         while j < n - 1:
             k = j + 1
+
             while k < n:
+                #combine items to give item pair name
                 item_pair = data['stimuli'][j] + '_AND_' + data['stimuli'][k]
+
+                #if item pair hasn't been encountered yet then create an entry for the pair
                 if item_pair not in myDict:
                     myDict[item_pair] = []
 
+                #append the distance for this pair to the list of distances for this pair
                 myDict[item_pair].append(data["rdmutv"][0][m])
                 k += 1
                 m += 1
             j += 1
 
-    # print(myDict)
 
     first_item_list = []
     second_item_list = []
     mean_distance_list = []
     SD_list = []
     category_list = []
+
+    #for each pair of items, fill their data into the above lists
     for key in myDict:
 
+        #if the item pair exists but in reverse order then we have an error in our data
         if key.split('_AND_')[1] + '_AND' + key.split('_AND_')[0] in myDict:
             print('ERROR WITH DATA - reversed pairs exist. raw data files likely contain items in different orders')
             exit()
+
+
         category_list.append(category)
         first_item_list.append(key.split('_AND_')[0])
         second_item_list.append(key.split('_AND_')[1])
         mean_distance_list.append(mean(myDict[key]))
         SD_list.append(stdev(myDict[key]))
 
+    #fill in data frames
     data = {'Category': category_list,
             'Unique_pair_item1': first_item_list,
             'Unique_pair_item2': second_item_list,
@@ -85,6 +101,8 @@ for directory_path in categories:
 
     df = pd.DataFrame(data, columns=['Category', 'Unique_pair_item1', 'Unique_pair_item2', 'Distance_m', 'Distance_sd'])
 
+
+    #export data frames to output file
     if flag == 1:
         df.to_csv("inputs/cleandata/distance_data.csv", index=False, mode='a', index_label=False, header=False)
     else:
--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# This is a script that visualizes the distances between items of each category
 
 
 import matplotlib.pyplot as plt
@@ -10,56 +7,58 @@ import pandas as pd
 import seaborn as sns
 from scipy.cluster.hierarchy import dendrogram, linkage
 from scipy.spatial.distance import squareform
-from sklearn.manifold import MDS
+import networkx as nx
 
+#import data
 df = pd.read_csv('inputs/cleandata/distance_data.csv')
-print(df)
 
+#list of categories
 category_list = df['Category'].unique()
-print(category_list)
 
+#create visualizations for each category
 for category in category_list:
-    print('\n\n\n')
-    print('Category: ' + category + '\n')
+
+    #get distance info for this category
     current_category_data = df.loc[df['Category'] == category, ['Unique_pair_item1', 'Unique_pair_item2', 'Distance_m']]
 
+    #get unique items
     unique_items_in_category = (current_category_data['Unique_pair_item1'].append(current_category_data['Unique_pair_item2'])).unique()
-    print(current_category_data)
-
-    print('Unique items: ')
-    print(unique_items_in_category)
-
     unique_item_count = len(unique_items_in_category)
 
+    #zero distance matrix of the appropriate size
     distance_matrix = np.zeros(shape=(unique_item_count, unique_item_count))
-   # print(distance_matrix)
-    #print(type(distance_matrix))
 
+    #replace half triangle of the matrix with NaNs
     distance_matrix[np.triu_indices(distance_matrix.shape[0], 1)] = np.nan
-    print(distance_matrix)
 
+    #make a dataframe using the above numpy matrix
     matrix_df = pd.DataFrame(data=distance_matrix, index=unique_items_in_category, columns=unique_items_in_category)
-    print(matrix_df)
 
+    #create another dataframe for the force-directed graph
     matrix_df_for_force_graph = matrix_df.copy()
 
+    #fill the dataframes with the measurement data for this category
     for index, row in current_category_data.iterrows():
+
+        #most of the visualizations can use a symmetric distance matrix (dataframe)
         matrix_df.at[row['Unique_pair_item1'], row['Unique_pair_item2']] = row['Distance_m']
         matrix_df.at[row['Unique_pair_item2'], row['Unique_pair_item1']] = row['Distance_m']
 
+        #the force-directed graph requires a threshold. relationships whose distance falls under the threshold will be drawn
+        #without a threshold, the graph becomes a web connecting all items
         if row['Distance_m'] < 0.07:
             matrix_df_for_force_graph.at[row['Unique_pair_item1'], row['Unique_pair_item2']] = row['Distance_m']
             matrix_df_for_force_graph.at[row['Unique_pair_item2'], row['Unique_pair_item1']] = row['Distance_m']
         else:
+            #if the distance between two items is too large, then set their connecting edge, or relationship to 0 (non-existent)
             matrix_df_for_force_graph.at[row['Unique_pair_item1'], row['Unique_pair_item2']] = 0
             matrix_df_for_force_graph.at[row['Unique_pair_item2'], row['Unique_pair_item1']] = 0
 
-    #print(matrix_df)
 
-    #plt heatmap
+    #heatmap (matplotlib)
     myindex = unique_items_in_category
     columns = unique_items_in_category
-    plt.pcolor(matrix_df)#, cmap='seismic'
+    plt.pcolor(matrix_df)
     plt.yticks(np.arange(0.5, len(myindex), 1), myindex)
     plt.xticks(np.arange(0.5, len(columns), 1), columns, rotation='vertical')
     plt.colorbar()
@@ -69,6 +68,7 @@ for category in category_list:
     plt.subplots_adjust(left = 0.262, bottom = 0.26, right = 0.857)
     plt.savefig('./figures/'+category + '_heatmap.png')
     plt.show()
+
 
     #dendrogram
     dists = squareform(matrix_df)
@@ -80,25 +80,17 @@ for category in category_list:
     plt.savefig('./figures/'+category + '_dendrogram.png')
     plt.show()
 
-    #seaborn cluster + dendrogram
-    g = sns.clustermap(matrix_df)
 
-    #plt.title(category + " dendrogram and clustered heatmap")
+    #cluster + dendrogram (seaborn)
+    g = sns.clustermap(matrix_df)
     plt.savefig('./figures/' + category + '_dendrogram_and_clustered_heat_map.png')
     plt.show()
 
 
-
     #force-directed graph
-    import networkx as nx
-
     A = matrix_df
     G = nx.from_pandas_adjacency(matrix_df_for_force_graph)
-
     pos = nx.kamada_kawai_layout(G, weight='weight')
-
     nx.draw_kamada_kawai(G, with_labels=True)
-
     plt.savefig('./figures/' + category + '_force_directed_graph.png')
-
     plt.show()
